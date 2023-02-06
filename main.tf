48bb0ea54f6e607a9f64bb51d921e1e41fc9949d
tony <tony.gibbs@orasi.com>
2023-02-06 16:26:00 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -18,8 +18,7 @@ resource "aws_vpc" "hashicat" {
   tags = {
     name = "${var.prefix}-vpc-${var.region}"
     environment = "Production"
-    Department = "devops"
-    Billable ="true"
+   
   }
 }
 
@@ -130,6 +129,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+     Department = "devops"
+    Billable ="true"
   }
 }
 
